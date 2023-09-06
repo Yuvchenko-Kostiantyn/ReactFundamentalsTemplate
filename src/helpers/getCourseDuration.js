1fc5a123a257982099c93f649affa6ff3e7c51dc
Kostiantyn Yuvchenko <kostiantyn.yuvchenko_epam@kantar.com>
2023-09-06 23:40:18 +0300

    Fix getCourseDuration helper one again

--- src/helpers/getCourseDuration.js
+++ src/helpers/getCourseDuration.js
@@ -2,7 +2,7 @@ export const getCourseDuration = (duration) => {
 	const minutes = duration % 60;
 	const hours = Math.floor(duration / 60);
 
-	return `${hours}:${minutes >= 10 ? minutes : '0' + minutes} ${
-		hours > 1 ? 'hours' : 'hour'
-	}`;
+	return `${hours >= 10 ? hours : '0' + hours}:${
+		minutes >= 10 ? minutes : '0' + minutes
+	} ${hours > 1 ? 'hours' : 'hour'}`;
 };
